19ce5409e2a01863976df983fb19f6bb99fbb5f9
xiaoyao-NKU <18946090101@163.com>
2026-01-22 13:31:01 +0800

    fix for pre-commot

--- tilelang/language/__init__.py
+++ tilelang/language/__init__.py
@@ -132,6 +132,7 @@ from .pdl import (
 )
 from . import comm  # noqa: F401
 
+
 def import_source(source: str | None = None):
     # source is the source code to be imported
     return block_attr({"pragma_import_c": source}) if source is not None else None
