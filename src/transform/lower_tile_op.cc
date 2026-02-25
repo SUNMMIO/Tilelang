6a0a64d838af68713b7f97294554ea9a121e8ba5
Jiaqi Guo <guojiaqi@sunmmio.com>
2026-02-25 20:37:57 +0800

    Fix layout map in lower_tile_op

--- src/transform/lower_tile_op.cc
+++ src/transform/lower_tile_op.cc
@@ -442,6 +442,10 @@ private:
           buffer->strides, buffer->elem_offset, buffer->name,
           buffer->data_alignment, buffer->offset_factor, buffer->buffer_type);
       return BufferLoad(new_buffer, load->indices);
+    } else if (!TargetIsSunmmio(target_)) {
+      // Sunmmio doesn't remap buffer, so we need to keep the buffer's layout in
+      // layout remap. Otherwise, the layout is lost in the follow-up transform.
+      layout_remap_.Set(buffer, layout_map_[load->buffer]);
     }
     return load;
   }
@@ -460,6 +464,10 @@ private:
           buffer->strides, buffer->elem_offset, buffer->name,
           buffer->data_alignment, buffer->offset_factor, buffer->buffer_type);
       return BufferStore(new_buffer, store->value, store->indices);
+    } else if (!TargetIsSunmmio(target_)) {
+      // Sunmmio doesn't remap buffer, so we need to keep the buffer's layout in
+      // layout remap. Otherwise, the layout is lost in the follow-up transform.
+      layout_remap_.Set(buffer, layout_map_[store->buffer]);
     }
     return store;
   }
